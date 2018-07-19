1fee50b66f560cd0399079fb3c86f538956271b6
intern-svrdc69 <xinyao.wang@jd.com>
2018-07-19 14:24:38 -0700

    fixed test code bug when vgg16 return is a list, removed usegpu option so that torch will automatically adjust which device to put coord channels

--- test.py
+++ test.py
@@ -1,95 +1,92 @@
+from __future__ import division, print_function
 import torch
 from torch.autograd import Variable
 from coord_conv import AddCoordinates, CoordConv, CoordConvTranspose, \
     CoordConvNet
+import pdb
 
 
 def generate_input(batch_size, image_height, image_width, usegpu=False):
 
-    input_image = torch.rand(batch_size, 3, image_height, image_width)
-    input_image = Variable(input_image)
     if usegpu:
-        input_image = input_image.cuda()
+        device = torch.device("cuda:0")
+    else:
+        device = torch.device("cpu")
+    input_image = torch.rand(batch_size, 3, image_height, image_width)
+    input_image.to(device)
 
     return input_image
 
 
-def test_addCoordinates(input_image, usegpu=False):
+def test_addCoordinates(input_image):
 
-    print '- AddCoordinates'
+    print('- AddCoordinates')
 
-    coord_adder = AddCoordinates(with_r=True, usegpu=usegpu)
+    coord_adder = AddCoordinates(with_r=True)
 
     output = coord_adder(input_image)
 
-    print '* Y :\n', output[0, 0]
-    print '* X :\n', output[0, 1]
-    print '* R :\n', output[0, 2]
+    print('* Y :\n', output[0, 0])
+    print('* X :\n', output[0, 1])
+    print('* R :\n', output[0, 2])
 
-    print '- AddCoordinates: OK!'
+    print('- AddCoordinates: OK!')
 
 
-def test_coordConv(input_image, usegpu=False):
+def test_coordConv(input_image):
 
-    print '- CoordConv'
+    print('- CoordConv')
 
-    coord_conv = CoordConv(3, 64, 3, with_r=True, usegpu=usegpu)
-    if usegpu:
-        coord_conv = coord_conv.cuda()
+    coord_conv = CoordConv(3, 64, 3, with_r=True)
     output = coord_conv(input_image)
 
-    print 'Input Size  : ', input_image.size()
-    print 'Output Size : ', output.size()
+    print('Input Size  : ', input_image.size())
+    print('Output Size : ', output.size())
 
-    print '- CoordConv: OK!'
+    print('- CoordConv: OK!')
 
 
-def test_coordConvTranspose(input_image, usegpu=False):
+def test_coordConvTranspose(input_image):
 
-    print '- CoordConvTranspose'
+    print('- CoordConvTranspose')
 
-    coord_conv_transpose = CoordConvTranspose(3, 64, 3, with_r=True,
-                                              usegpu=usegpu)
-    if usegpu:
-        coord_conv_transpose = coord_conv_transpose.cuda()
+    coord_conv_transpose = CoordConvTranspose(3, 64, 3, with_r=True)
     output = coord_conv_transpose(input_image)
 
-    print 'Input Size  : ', input_image.size()
-    print 'Output Size : ', output.size()
+    print('Input Size  : ', input_image.size())
+    print('Output Size : ', output.size())
 
-    print '- CoordConvTranspose: OK!'
+    print('- CoordConvTranspose: OK!')
 
 
-def test_coordConvNet(usegpu=False):
+def test_coordConvNet():
 
-    print '- CoordConvNet'
+    print('- CoordConvNet')
 
     import torchvision.models as models
 
     vgg16 = models.__dict__['vgg16'](pretrained=False)
 
-    print 'VGG16 :\n', vgg16
+    print('VGG16 :\n', vgg16)
 
-    vgg16 = CoordConvNet(vgg16, with_r=True, usegpu=usegpu)
+    vgg16 = CoordConvNet(vgg16, with_r=True)
 
-    print 'CoordVGG16 :\n', vgg16
+    print('CoordVGG16 :\n', vgg16)
 
-    if usegpu:
-        vgg16 = vgg16.cuda()
 
     output = vgg16(input_image)
 
-    print 'Input Size  : ', input_image.size()
-    print 'Output Size : ', output.size()
+    print('Input Size  : ', input_image.size())
+    print('Output Size : ', [i.size() for i in output])
 
-    print '- CoordConvNet: OK!'
+    print('- CoordConvNet: OK!')
 
 if __name__ == '__main__':
-    usegpu = False
 
+    usegpu=True
     input_image = generate_input(2, 64, 64, usegpu=usegpu)
 
-    test_addCoordinates(input_image, usegpu)
-    test_coordConv(input_image, usegpu)
-    test_coordConvTranspose(input_image, usegpu)
-    test_coordConvNet(usegpu)
+    test_addCoordinates(input_image)
+    test_coordConv(input_image)
+    test_coordConvTranspose(input_image)
+    test_coordConvNet()
